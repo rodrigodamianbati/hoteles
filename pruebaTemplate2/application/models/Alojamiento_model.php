a1c01880a7f5513d7a0ebbed4fccded5e3167450
rodrigodamianbati <r.batillier@gmail.com>
2018-10-26 17:03:41 -0300

    Probando proyecto

--- pruebaTemplate2/application/models/Alojamiento_model.php
+++ pruebaTemplate2/application/models/Alojamiento_model.php
@@ -2,10 +2,25 @@
                
                //extendemos CI_Model
 class Alojamiento_model extends CI_Model{
+        /*
+        $this->db->from('alojamiento as a');
+        $this->db->join('estado_aloj as ea','a.id_estado=ea.id');
+        $this->db->join('servicio_aloj as sa','a.id=sa.id_alojamiento');
+        $this->db->join('servicio as s','s.id=sa.id_servicio');
+        $this->db->join('tipo_aloj as ta','a.id_tipo=ta.id');
+        $this->db->join('foto_alojamiento as fa','a.id=fa.id_alojamiento');
+        */
+        //private $estado = 'Estado_model';
+        //private $servicios;
+
+
 
     public function __construct() {
         //llamamos al constructor de la clase padre
         parent::__construct(); 
+
+        //$this->load->model('Estado_model');
+        $this->load->model('Alojamiento_model');
         
     }
 
@@ -68,6 +83,7 @@ class Alojamiento_model extends CI_Model{
         return $this->db->get('alojamiento')->num_rows();
     }
 
+    /*
     public function alojamientos($limite){
         //$consulta=$this->db->query("SELECT * FROM alojamiento a JOIN servicio_aloj sa ON (a.id=sa.id_alojamiento) JOIN estado_aloj ea ON (a.id=ea.id_alojamiento) JOIN foto_alojamiento fa ON (a.id=fa.id_alojamiento)");
         $this->db->select('*');
@@ -86,10 +102,71 @@ class Alojamiento_model extends CI_Model{
         $consulta= $this->db->get('alojamiento', $limite, $this->uri->segment(3));
         //--------------------->CASTEAR OBJETOS
         return $consulta->result();
-    }
-    
-    public function alojamiento(){
+    } */
+
+    public function alojamientos($limite){
         
+        $consulta = $this->db->query("SELECT * FROM alojamiento;");
+        
+        foreach ($consulta->result('Alojamiento_model') as $row) {
+            //$row->id = $row->id; // call attributes
+            $row->id = $row->id;
+            $row->precio = $row->precio;
+            $row->id_localidad = $row->localidad($row->id_localidad);
+            $row->direccion_nombre = $row->direccion_nombre;
+            $row->direccion_numero = $row->direccion_numero;
+            $row->id_estado = $row->estado($row->id_estado);
+            $row->id_usuario = $row->usuario($row->id_usuario);
+            $row->id_tipo = $row->tipo($row->id_tipo);
+
+            //print_r($row);
+            //die();
+        } 
+        
+        //print_r($consulta->result('Alojamiento_model'));
+        //die();
+
+        /*
+        foreach ($consulta->result('Alojamiento_model') as $row) {
+            //$row->id = $row->id; // call attributes
+            print_r($consulta);
+            die();
+        }
+
+        print_r($consulta->result());
+        die();
+        */
+
+        return $consulta->result('Alojamiento_model');
+    }
+    /*   $query = $this->db->query("SELECT * FROM users;");
+   
+    foreach ($query->result('User') as $row)
+    {
+        echo $row->name; // call attributes
+        echo $row->reverse_name(); // or methods defined on the 'User' class
+    }
+    */
+
+    
+    public function localidad($id_localidad){
+        $consulta = $this->db->query("SELECT * FROM localidad l WHERE l.id='$id_localidad';");
+        return $consulta->result();
+    }
+
+    public function estado($id_estado){
+        $consulta = $this->db->query("SELECT * FROM estado_aloj ea WHERE ea.id='$id_estado'");
+        return $consulta->result();
+    }
+
+    public function usuario($id_usuario){
+        $consulta = $this->db->query("SELECT * FROM usuario u WHERE u.id='$id_usuario'");
+        return $consulta->result();
+    }
+
+    public function tipo($id_tipo){
+        $consulta = $this->db->query("SELECT * FROM tipo_aloj t WHERE t.id='$id_tipo'");
+        return $consulta->result();
     }
 }
 
